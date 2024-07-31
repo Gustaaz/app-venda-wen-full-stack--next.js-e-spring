995c106b8ec272091759e5b9d59d107a95500ef7
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-07-31 09:31:56 -0400

    escopo base do projeto web

---
+++ web-vendas/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from 'next-themes'
+import { Toaster } from '@/components/ui'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Web-Vendas',
+  description: 'Web-Vendas',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
