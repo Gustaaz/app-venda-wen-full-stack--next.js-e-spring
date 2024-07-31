a5c0322942ca7839fe310f5cb331b57f86d8f9c5
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-07-31 09:20:18 -0400

    chamada api de produtos para buscar por id

---
+++ web-vendas/src/actions/product/get-by-id.ts
@@ -0,0 +1,17 @@
+'use server'
+import { Product } from '@/types/product'
+import { resourceURLProduct } from '@/ultls/resouce-url-product'
+
+export const getById = async (id: string): Promise<Product> => {
+  const response = await fetch(`${resourceURLProduct}/${id}`, {
+    method: 'GET',
+  })
+
+  if (!response.ok) {
+    throw new Error('Ocorreu um erro inesperado')
+  }
+
+  const product = (await response.json()) as Promise<Product>
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return product
+}
