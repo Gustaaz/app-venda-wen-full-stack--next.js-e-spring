ce93bc960b6c07a54370a0ca84939b953110b6b3
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-08-01 11:07:34 -0400

    alterando o input para receber um formataador de mascara

--- web-vendas/src/components/ui/input.tsx
+++ web-vendas/src/components/ui/input.tsx
@@ -7,10 +7,19 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
   error?: string
+  formatter?: (value: string, name: string) => void
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, error, ...props }, ref) => {
+  ({ className, type, label, error, formatter, onChange, ...props }, ref) => {
+    function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+      const { value, name } = event.target
+
+      const formattedValue = (formatter && formatter(value, name)) || value
+      console.log(formattedValue)
+
+      event.target.value = formattedValue
+    }
     return (
       <div className="grid w-full gap-2">
         {label && (
@@ -22,6 +31,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 
         <input
           type={type}
+          onChange={onInputChange}
           className={cn(
             'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
             className,
