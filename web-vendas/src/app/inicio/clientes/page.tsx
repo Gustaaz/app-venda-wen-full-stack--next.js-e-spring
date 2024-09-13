fa496043747d7abb283d0cef41e9db0ecd360881
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-13 11:17:13 -0400

    removendo comentarios

--- web-vendas/src/app/inicio/clientes/page.tsx
+++ web-vendas/src/app/inicio/clientes/page.tsx
@@ -44,27 +44,6 @@ export default async function Customers({
           </CardHeader>
           <CardContent>
             <DataTableCustomer pageCustomer={customers} />
-
-            {/* {customers.content.length === 0 && (
-              <div className="flex h-96 flex-1 items-center justify-center rounded-lg shadow-sm">
-                <div className="flex flex-col items-center gap-1 text-center">
-                  <h3 className="text-2xl font-bold tracking-tight">
-                    Você não possui nenhum cliente
-                  </h3>
-                  <p className="text-sm text-muted-foreground">
-                    Clique no botão abaixo para adicionar um
-                  </p>
-                  <Link href="/inicio/clientes/cadastro">
-                    <Button className="mt-2 gap-1">
-                      <PlusCircle className="h-3.5 w-3.5" />
-                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Cadastrar Cliente
-                      </span>
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-            )} */}
           </CardContent>
         </Card>
       </div>
